4385cd9ba308013396ace227fe20e148a98eceef
lucas <zlucaschipolariz@gmail.com>
2024-04-17 08:29:49 -0300

    Dividi la agenda y el contacto en dos clases diferentes

---
+++ Ejercicio_7/Contacto.js
@@ -0,0 +1,11 @@
+export class Contacto{
+    constructor(nombre, telefono){
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+    mostrarContacto(){
+        alert(`Nombre: ${e.nombre}, Teléfono: ${e.telefono}\n`);
+    }
+  
+
+}
--- Ejercicio_7/index.js
+++ Ejercicio_7/index.js
@@ -17,69 +17,8 @@ huecosLibres(): indica cuántos contactos más podemos ingresar.
 
 Crea un menú con opciones que serán seleccionadas por el usuario usando un prompt, las salidas de las operaciones seleccionadas por el usuario se pueden mostrar en pantalla y  por consola.
 */
-
-class Agenda{
-    constructor(tamaño=10){
-        this.contactos = [];
-        this.tamaño = tamaño;
-    }
-    aniadirContacto(contacto){
-        if(this.contactos.length < this.tamaño){
-            this.contactos.push(contacto);
-        }else{1
-            console.log('La agenda no puede almacenar más contactos');
-        }
-    }
-    existeContacto(nombre){
-        if( this.contactos.find(e => e.nombre === nombre)){
-            return true;
-        }else{return false}
-       /* this.contactos.forEach(e => {
-            if(e.nombre === nombre){
-                alert('El contacto si existe');
-            }
-        });*/
-    }
-    listarContactos(){
-        return this.contactos;3
-    }
-    buscarContacto(nombre){
-        this.contactos.forEach(e =>{
-            if(e.nombre === nombre){
-               alert(`El telefono del contacto es: ${e.telefono}`);
-            }   
-        });
-    }
-    agendaLlena(){
-        if(this.contactos.length === this.tamaño){
-            return true;
-        }else{
-            return false;
-        }
-    }
-    huecosLibres(){
-        return 10-this.contactos.length;
-    }
-    eliminarContacto(contacto){ 
-        this.contactos.forEach(e=> {
-            if(e.nombre === contacto.nombre || e.telefono === contacto.telefono){
-                this.contactos.splice(this.contactos.indexOf(e), 1);
-            }
-        });
-    }
-}
-class Contacto{
-    constructor(nombre, telefono){
-        this.nombre = nombre;
-        this.telefono = telefono;
-    }
-    mostrarContacto(){
-        alert(`Nombre: ${e.nombre}, Teléfono: ${e.telefono}\n`);
-    }
-  
-
-}
-
+import {Agenda} from './Agenda.js';
+import {Contacto} from './Contacto.js';
 const agenda = new Agenda();
 
 let opcion;
